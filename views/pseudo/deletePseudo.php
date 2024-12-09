f5953c264f33668deecc3053f28b8b0f8e9ae3c4
Gfeller <jeremy.gfeller@hotmail.com>
2024-12-09 12:30:21 +0100

    Create deletePseudo

---
+++ views/pseudo/deletePseudo.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once '../../controllers/PseudoController.php';
+$pseudoController = new PseudoController();
+
+if (isset($_GET['idPseudo'])) {
+    $id = $_GET['idPseudo'];
+    $pseudoController->delete($id);
+}
