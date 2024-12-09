41ae2fc0816b3ece5f5e8ea6ef30def3fef49603
Gfeller <jeremy.gfeller@hotmail.com>
2024-12-09 10:43:15 +0100

    Create functions to add, udpate or remove a game in pseudos_in_videogames

--- models/PseudoInVideoGame.php
+++ models/PseudoInVideoGame.php
@@ -9,12 +9,52 @@ class PseudoInVideoGame
         $this->database = new Database();
     }
 
-    public function addPseudoInVideoGame($idPseudo, $idVideogame)
+    public function getVideogameByPseudoId($fkPseudo)
+    {
+        $query = 'select * from pseudos_in_videogames where fkPseudo = :fkPseudo';
+        $binds = [
+            "fkPseudo" => $fkPseudo,
+        ];
+
+        $req = $this->database->queryPrepareExecute($query, $binds);
+        return $this->database->formatDataAssoc($req);
+    }
+
+    public function isInTable($fkPseudo, $fkVideogame)
+    {
+        $query = 'select * from pseudos_in_videogames where fkPseudo = :fkPseudo and fkVideogame = :fkVideogame';
+        $binds = [
+            "fkPseudo" => $fkPseudo,
+            "fkVideogame" => $fkVideogame,
+        ];
+
+        $req = $this->database->queryPrepareExecute($query, $binds);
+        $res = $this->database->formatOneData($req);
+
+        if (empty($res)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function addPseudoInVideoGame($fkPseudo, $fkVideogame)
     {
         $query = "INSERT INTO pseudos_in_videogames (fkPseudo, fkVideogame) VALUES (:fkPseudo, :fkVideogame)";
         $binds = [
-            "fkPseudo" => $idPseudo,
-            "fkVideogame" => $idVideogame,
+            "fkPseudo" => $fkPseudo,
+            "fkVideogame" => $fkVideogame,
+        ];
+
+        $this->database->queryPrepareExecute($query, $binds);
+    }
+
+    public function deleteVideoGameByIds($fkPseudo, $fkVideogame)
+    {
+        $query = "DELETE FROM pseudos_in_videogames WHERE fkPseudo = :fkPseudo AND fkVideogame = :fkVideogame;";
+        $binds = [
+            "fkPseudo" => $fkPseudo,
+            "fkVideogame" => $fkVideogame,
         ];
 
         $this->database->queryPrepareExecute($query, $binds);
